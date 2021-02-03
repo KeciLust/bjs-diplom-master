49ac69ef2bc548c0f4f0f4071001bab4f391d5e2
Denis Nikitin <dinya.nikitin@gmail.com>
2021-02-03 17:38:00 +0300

    Finish change 2

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,13 +3,13 @@
 const userForm = new UserForm();
 userForm.loginFormCallback = data => {
   ApiConnector.login(data, result => {
-  
-    return result.success ? location.reload() : userForm.setLoginErrorMessage(`Неправильный логин или пароль`);
+  console.log(result)
+    return result.success ? location.reload() : userForm.setLoginErrorMessage(result.error);
 
   });
 }
 userForm.registerFormCallback = data => {
   ApiConnector.register(data, result => {
-    return result.success ? location.reload() : userForm.setRegisterErrorMessage(`Не все поля заполнены`);
+    return result.success ? location.reload() : userForm.setRegisterErrorMessage(result.error);
   });
 }
