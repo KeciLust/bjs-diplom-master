ce65971340a00632f9155afb2bf3143aca8d8979
Denis Nikitin <dinya.nikitin@gmail.com>
2021-02-03 14:03:07 +0300

    Finish

---
+++ public/homePage.js
@@ -0,0 +1,110 @@
+const logout = new LogoutButton();
+logout.action = () => {
+    ApiConnector.logout(result => {
+        if (result.success) {
+            return location.reload();
+        }
+    });
+}
+ApiConnector.current(result => {
+    if (result.success) {
+        return ProfileWidget.showProfile(result.data);
+    }
+});
+
+
+
+
+const ratesBoard = new RatesBoard();
+
+function newRatesBoard() {
+    ApiConnector.getStocks(result => {
+        if (result.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(result.data);
+        }
+    });
+}
+newRatesBoard();
+setInterval(newRatesBoard(), 60000);
+
+
+
+
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+    ApiConnector.addMoney(data, (result) => {
+        let message;
+        if (result.success) {
+            ProfileWidget.showProfile(result.data);
+            message = `Операция выполнена`;
+        } else {
+            message = `Операция не выполнена`;
+        }
+        return moneyManager.setMessage(result.success, message);
+
+    })
+}
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, (result) => {
+        let message;
+        if (result.success) {
+            ProfileWidget.showProfile(result.data);
+            message = `Операция выполнена`;
+        } else {
+            message = `Операция не выполнена`;
+        }
+        return moneyManager.setMessage(result.success, message);
+    })
+}
+
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, (result) => {
+        let message;
+        if (result.success) {
+            ProfileWidget.showProfile(result.data);
+            message = `Операция выполнена`;
+        } else {
+            message = `Операция не выполнена`;
+        }
+         moneyManager.setMessage(result.success, message);
+    })
+}
+
+
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites(result => {
+    if(result.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(result.data);
+        moneyManager.updateUsersList(result.data);
+    }
+})
+favoritesWidget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, (result) => {
+        let message;
+     if(result.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(result.data);
+        moneyManager.updateUsersList(result.data);
+        message = `Операция выполнена`;
+     } else {
+         message = `Операция не выполнена`;
+     } favoritesWidget.setMessage(result.success, message);
+    })
+}
+favoritesWidget.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, (result) => { 
+        let message;
+        if(result.success) {
+           favoritesWidget.clearTable();
+           favoritesWidget.fillTable(result.data);
+           moneyManager.updateUsersList(result.data);
+           message = `Операция выполнена`;
+        } else {
+            message = `Операция не выполнена`;
+        } favoritesWidget.setMessage(result.success, message);
+    })
+
+}
+
--- public/loginPage.js
+++ public/loginPage.js
@@ -3,11 +3,13 @@
 const userForm = new UserForm();
 userForm.loginFormCallback = data => {
   ApiConnector.login(data, result => {
-    return result.success ? location.reload() : alert ("Ошибка");
-  } );
+  
+    return result.success ? location.reload() : userForm.setLoginErrorMessage(`Неправильный логин или пароль`);
+
+  });
 }
 userForm.registerFormCallback = data => {
   ApiConnector.register(data, result => {
-    return result.success ? location.reload() : alert ("Ошибка");
-  })
+    return result.success ? location.reload() : userForm.setRegisterErrorMessage(`Не все поля заполнены`);
+  });
 }
